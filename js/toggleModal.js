a53ad222cec666bd5921da22991d58683454cdce
Martin Li <80705619+MartinLilt@users.noreply.github.com>
2022-10-02 17:52:22 +0300

    mobile-v-1.00

---
+++ js/toggleModal.js
@@ -0,0 +1,15 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-open]"),
+    closeModalBtn: document.querySelector("[data-close]"),
+    modal: document.querySelector("[data-modal-menu]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    console.log("toggel");
+    refs.modal.classList.toggle("is-open");
+  }
+})();
