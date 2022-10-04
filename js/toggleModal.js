969298414ff3e7fac4cc711a83b15ba4c7f3a97a
Martin Li <80705619+MartinLilt@users.noreply.github.com>
2022-10-05 00:40:15 +0300

    mobile-1.0.2

--- js/toggleModal.js
+++ js/toggleModal.js
@@ -3,13 +3,14 @@
     openModalBtn: document.querySelector("[data-open]"),
     closeModalBtn: document.querySelector("[data-close]"),
     modal: document.querySelector("[data-modal-menu]"),
+    bodyOverFlowHidden: document.querySelector("[data-body-hidden]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    console.log("toggel");
     refs.modal.classList.toggle("is-open");
+    refs.bodyOverFlowHidden.classList.toggle("is-hidden");
   }
 })();
